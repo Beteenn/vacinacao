1b05e33786de17e70c7ab6b758723f771165d241
Gilberto Barbosa <gilbertoaraujo229@gmail.com>
2024-04-21 15:15:49 -0300

    FIX: Cria camada de IoC e classes de configuracoes nela e remove da program

---
+++ CartaoVacina.IoC/CartaoVacinaApplicationModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CartaoVacina.IoC;
+
+public static class CartaoVacinaApplicationModule
+{
+    public static IServiceCollection ConfigureDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.ConfigureDataBase(configuration);
+        services.ConfigureRepositories();
+        services.ConfigureServices();
+        services.ConfigureSwagger();
+
+        return services;
+    }
+}
