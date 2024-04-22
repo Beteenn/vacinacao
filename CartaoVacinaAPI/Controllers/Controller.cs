b988425caacaba6767d7d33e6f59b7114776b951
Gilberto Barbosa <gilbertoaraujo229@gmail.com>
2024-04-21 21:00:04 -0300

    FEATURE: Cria controller base para tratar os resultados do tipo result

---
+++ CartaoVacinaAPI/Controllers/Controller.cs
@@ -0,0 +1,16 @@
+﻿using CartaoVacina.Core.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartaoVacina.API.Controllers
+{
+    public abstract class Controller : ControllerBase
+    {
+        protected IActionResult TratarResultado(Result result)
+        {
+            if (!result)
+                return BadRequest(result);
+
+            return Ok();
+        }
+    }
+}
