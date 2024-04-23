eda7e84d018c7c0cc43f626df1fa48d57e1f5ed0
Gilberto Barbosa <gilbertoaraujo229@gmail.com>
2024-04-22 21:32:36 -0300

    FEATURE: Adiciona Validadores de aplicacao de dose

--- CartaoVacina.Core/Entities/VacinacaoCollection.cs
+++ CartaoVacina.Core/Entities/VacinacaoCollection.cs
@@ -1,64 +1,42 @@
-﻿using CartaoVacina.Core.Results;
+﻿using CartaoVacina.Core.Enums;
+using CartaoVacina.Core.Results;
+using CartaoVacina.Core.Validators;
 
 namespace CartaoVacina.Core.Entities;
 
-public class VacinacaoCollection : List<Vacinacao>
+public sealed class VacinacaoCollection : List<Vacinacao>
 {
-    private Vacinacao? ObterUltimaVacinacaoPorVacinaId(long vacinaId)
+    private readonly Dictionary<TipoDose, IValidadorDose> _validadores = new()
+    {
+        {TipoDose.Comum, new ValidadorDoseComum() },
+        {TipoDose.Reforco, new ValidadorDoseReforco() },
+    };
+
+    private IValidadorDose ObterValidador(TipoDose tipoDose)
+    {
+        _validadores.TryGetValue(tipoDose, out var validador);
+
+        return validador;
+    }
+
+    internal Vacinacao? ObterUltimaVacinacaoPorTipoEId(long vacinaId, TipoDose tipoDose = TipoDose.Comum)
     {
         return this
             .OrderByDescending(x => x.NumeroDose)
-            .FirstOrDefault(x => x.VacinaId == vacinaId);
+            .FirstOrDefault(x => x.VacinaId == vacinaId && x.TipoDose == tipoDose);
     }
 
     private Vacinacao? ObterPorId(long vacinacaoId)
         => this.FirstOrDefault(x => x.Id == vacinacaoId);
 
-    private Result ValidarAplicacao(Vacinacao vacinacao, int numeroDose)
+    internal Result AdicionarVacinacao(Vacina vacina, int numeroDose, DateTime dataAplicacao, TipoDose tipoDose)
     {
-        if (numeroDose > vacinacao.Vacina.QuantidadeDoses)
-            return Result.Fail("Vacina não possui esta dose.");
+        var aplicacaoValida = ObterValidador(tipoDose).ValidarAplicacao(this, vacina, numeroDose);
 
-        if (numeroDose == vacinacao.NumeroDose)
-            return Result.Fail("Esta dose ja foi aplicada.");
+        if (!aplicacaoValida)
+            return aplicacaoValida;
 
-        if (numeroDose > (vacinacao.NumeroDose + 1))
-            return Result.Fail("Há doses faltantes para esta vacina.");
-
-        return Result.Success;
-    }
-
-    private Result ValidarPrimeiraAplicacao(Vacina vacina, int numeroDose)
-    {
-        if (numeroDose > vacina.QuantidadeDoses)
-            return Result.Fail("Vacina não possui esta dose.");
-
-        if (numeroDose > 1)
-            return Result.Fail("Há doses faltantes para esta vacina.");
-
-        return Result.Success;
-    }
-
-    internal Result AdicionarVacinacao(Vacina vacina, int numeroDose, DateTime dataAplicacao)
-    {
-        var vacinacao = ObterUltimaVacinacaoPorVacinaId(vacina.Id);
-
-        if (vacinacao == null)
-        {
-            var aplicacaoValida = ValidarPrimeiraAplicacao(vacina, numeroDose);
-
-            if (!aplicacaoValida)
-                return aplicacaoValida;
-        }
-        else
-        {
-            var aplicacaoValida = ValidarAplicacao(vacinacao, numeroDose);
-
-            if (!aplicacaoValida)
-                return aplicacaoValida;
-        }
-
-        Add(Vacinacao.Create(vacina.Id, numeroDose, dataAplicacao));
+        Add(Vacinacao.Create(vacina.Id, numeroDose, dataAplicacao, tipoDose));
 
         return Result.Success;
     }
